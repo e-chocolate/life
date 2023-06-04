8ec1d277d347f9d46d9349a319f50f6c1382ae27
Echlorine <echo_taken@126.com>
2023-06-04 13:18:55 +0800

    update travel and sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,16 +7,42 @@ module.exports = {
     ],
     themeConfig: {
         logo: '/heart.svg', // 配置导航栏logo(themeConfig.logo)
+        author: 'Echlorine',
         nav: [
-          // 可指定链接跳转模式：默认target: '_blank'新窗口打开，_self当前窗口打开
-          { text: '首页', link: '/' },
-          { text: '急救', link: '/first_aid/'},
-          { text: '交通', link: '/traffic/' },
-          { text: '抑郁症', link: '/depression/' },
-          { text: '时间轴', link: '/timeLine/', icon: 'reco-date' },
-          { text: '关于', link: '/about/'},
-          { text: 'Github', link: 'https://github.com/Echlorine/life'}
+            // 可指定链接跳转模式：默认target: '_blank'新窗口打开，_self当前窗口打开
+            { text : '首页', link : '/' },
+            { text : '急救', link : '/first_aid/' },
+            { text : '抑郁症', link : '/depression/' },
+            {
+                text : '旅游',
+                items : [
+                    { text : '江苏', link : '/travel/JS/' },
+                    { text : '吉林', link : '/travel/JL/' }
+                ]
+            },
+            { text : '时间轴', link : '/timeLine/', icon : 'reco-date' },
+            { text : '关于', link : '/about/' },
+            { text : 'Github', link : 'https://github.com/Echlorine/life' }
         ],
-        sidebar: 'auto' // 全局设置自动生成侧边栏
+        sidebar: {
+            '/depression/' : [
+                ''
+            ],
+            '/first_aid/' : [
+                '',
+                'scenario'
+            ],
+            '/travel/JS/' : [
+                '',
+                'NKG',
+                'SZV',
+            ],
+            '/travel/JL/' : [
+                '',
+                'CGQ',
+                'YNJ',
+            ]
+        },
+        subSidebar: 'auto'
       }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,8 @@
 ---
 home: true
 heroImage: /heartPulse.svg
-heroText: 急救百科
-tagline: Echocolate 的医学急救科普指南
+heroText: 热爱生活
+tagline: 一花一世界，一沙一天国
 actionText: 快速了解 →
 actionLink: /first_aid/
 ---
